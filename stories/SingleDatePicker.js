eaf7edca1f5260f584c7775ce001209467ae0f5e
Alexander Bjerkan <alb@knowit.no>
2016-10-10 19:38:19 +0200

    Removed all other storybook examples

--- stories/SingleDatePicker.js
+++ stories/SingleDatePicker.js
@@ -4,48 +4,7 @@ import { storiesOf, action } from '@kadira/storybook';
 
 import SingleDatePickerWrapper from '../examples/SingleDatePickerWrapper';
 
-import { VERTICAL_ORIENTATION } from '../constants';
-
 storiesOf('SingleDatePicker', module)
-  .add('default', () => (
-    <SingleDatePickerWrapper />
-  ))
-  .add('single month', () => (
-    <SingleDatePickerWrapper
-      numberOfMonths={1}
-    />
-  ))
-  .add('vertical', () => (
-    <SingleDatePickerWrapper
-      orientation={VERTICAL_ORIENTATION}
-    />
-  ))
-  .add('horizontal with portal', () => (
-    <SingleDatePickerWrapper
-      withPortal
-    />
-  ))
-  .add('horizontal with fullscreen portal', () => (
-    <SingleDatePickerWrapper withFullScreenPortal />
-  ))
-  .add('vertical with full screen portal', () => (
-    <SingleDatePickerWrapper
-      orientation={VERTICAL_ORIENTATION}
-      withFullScreenPortal
-    />
-  ))
-  .add('non-english locale', () => {
-    moment.locale('zh-cn');
-    return (
-      <SingleDatePickerWrapper
-        placeholder="入住日期"
-        monthFormat="YYYY[年]MMMM"
-        phrases={{
-          closeDatePicker: '关闭',
-        }}
-      />
-    );
-  })
   .add('Norwegian DST trouble', () => {
     moment.locale('nb');
     return (
@@ -63,15 +22,4 @@ storiesOf('SingleDatePicker', module)
         </p>
       </div>
     );
-  })
-  .add('with custom display format', () => (
-    <SingleDatePickerWrapper
-      displayFormat="MMM D"
-    />
-  ))
-  .add('with outside days enabled', () => (
-    <SingleDatePickerWrapper
-      numberOfMonths={1}
-      enableOutsideDays
-    />
-  ));
+  });
